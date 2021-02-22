161042c53144174d1ffbf492bd26a6db426d5f35
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-02-22 23:31:03 +0530

    Update Pattern2.java

--- Basic/Pattern2.java
+++ Basic/Pattern2.java
@@ -4,8 +4,14 @@ public class Pattern1 {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=n;j>=i;j--){
+//         for(int i=1;i<=n;i++){
+//             for(int j=n;j>=i;j--){
+//                 System.out.print("*\t");
+//             }
+//             System.out.println();
+//         }  or
+          for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print("*\t");
             }
             System.out.println();
