48aed7654c30605566ae99a393a3155d6ac99287
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-02-26 14:42:57 +0530

    Update Pattern7.java

--- Basic/Pattern7.java
+++ Basic/Pattern7.java
@@ -32,3 +32,38 @@ public class Pattern7 {
 
 
 */
+
+
+// simple method
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void  main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+//        int st=1;
+//        int sp=0;
+        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=sp;j++){
+//                System.out.print("\t");
+//
+//            }
+//            for(int j=1;j<=st;j++){
+//                System.out.print("*\t");
+//            }
+//            sp++;
+//            System.out.println();
+            for(int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+}
+
