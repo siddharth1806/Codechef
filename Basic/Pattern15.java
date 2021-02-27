243b409df9ce6b9a7b801b624131f0135c2894d7
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-02-27 15:03:14 +0530

    Create Pattern15.java

---
+++ Basic/Pattern15.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Pattern15 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sp=n/2;
+        int st=1;
+        int val=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+
+            }
+            int cval=val;
+            for(int j=1;j<=st;j++){
+                System.out.print(cval + "\t");
+                if(j<=st/2) {
+                    cval++;
+                }else{
+                    cval--;
+                }
+            }
+
+            if(i<=n/2){
+                sp--;
+                st+=2;
+                val++;
+
+            }
+            else{
+                sp++;
+                st-=2;
+                val--;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+/*
+
+
+
+7
+			1	
+		2	3	2	
+	3	4	5	4	3	
+4	5	6	7	6	5	4	
+	3	4	5	4	3	
+		2	3	2	
+			1	
+      
+      
+ */
+
+      
+      
+      
+      
