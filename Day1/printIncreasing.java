cf7cab9f1e82b4a34c127e9bb22cabe9b2ef2733
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-07-25 15:24:16 +0530

    Update printIncreasing.java

--- Day1/printIncreasing.java
+++ Day1/printIncreasing.java
@@ -1,3 +1,27 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) throws Exception {
+//         // write your code here
+//          Scanner sc=new Scanner(System.in);
+//          int c=1;
+//         int n=sc.nextInt();
+//         printIncreasing(n,c);
+//     }
+
+//     public static void printIncreasing(int n,int c){
+          
+//         if(n<c){
+//             return;
+//         }
+      
+//         System.out.println(c);
+//         printIncreasing(n,c+1);
+//     }
+
+// }
 import java.io.*;
 import java.util.*;
 
@@ -6,19 +30,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // write your code here
          Scanner sc=new Scanner(System.in);
-         int c=1;
+       
         int n=sc.nextInt();
-        printIncreasing(n,c);
+        printIncreasing(n);
     }
 
-    public static void printIncreasing(int n,int c){
+    public static void printIncreasing(int n){
           
-        if(n<c){
+        if(n==0){
             return;
         }
       
-        System.out.println(c);
-        printIncreasing(n,c+1);
+        
+        printIncreasing(n-1);
+        System.out.println(n);
     }
 
 }
