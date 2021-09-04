bc12399107312bcd40279cd81e16cedfd869c8b4
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2021-09-04 12:29:24 +0530

    Update TargetSumSubset.java

--- dp/TargetSumSubset.java
+++ dp/TargetSumSubset.java
@@ -12,8 +12,9 @@ public class Main {
         }
         int tar=sc.nextInt();
         Boolean dp[][]=new Boolean[n][tar+1];
-        System.out.println(tss(arr,0,tar,dp));
+        System.out.println(tssTab(arr,tar));
     }
+    //memorization
     public static boolean tss(int[] arr, int idx, int tar, Boolean[][] dp){
         if(tar==0){
             return true;
@@ -31,4 +32,29 @@ public class Main {
         dp[idx][tar]=ans;
         return ans;
     }
+    //tabulation
+    public static boolean tssTab(int[] arr, int tar){
+        boolean[][] dp=new boolean[arr.length+1][tar+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=true;
+        }
+        
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                dp[i][j]=dp[i-1][j];
+                if( dp[i][j] == false && j-arr[i-1]>=0)
+                {
+                    dp[i][j]=dp[i-1][j-arr[i-1]];
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+    
+    
+    
+    
+    
+    
+    
 }
